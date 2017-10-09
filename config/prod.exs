f4226e7aea35159df10866529f0d99fc3d9e07f6
slama <s417.lama@gmail.com>
2017-10-09 15:23:38 +0900

    setup for docker

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :weblio_scraping, WeblioScrapingWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [host: "localhost", port: 80],
   server: true
 
 # Do not print debug messages in production
